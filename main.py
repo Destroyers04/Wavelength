11a3d437d71b83826cea6a23edc0330daf544720
Aaron Male <aaron@Aarons-MacBook-Pro.local>
2026-01-09 23:09:24 +0100

    Decided to use a csv file instead of a json file, and started on parsing said csv file

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import json
+import csv
+
+def csvParser():
+    with open("WavelengthCopy/Spectrums/default.csv", newline="") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ')
+        for row in spamreader:
+            print(row[0],"Middle", row[1])
 
 
 def main():
     print("The category is:")
+    csvParser()
 
 if __name__ == "__main__":
     main()
